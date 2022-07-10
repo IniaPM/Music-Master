c83c3fb8be022639b37179f07d92b6f105b5b140
Nick Do <linh.do@tyme.com>
2022-07-11 05:33:07 +0700

    fix compilation errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 
-const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com'
+const API_ADDRESS = "https://spotify-api-wrapper.appspot.com";
 
-class App extends Component
-  state = { artistQuery: '', artist: null, tracks: [] };
+function App() {
+  const [state, setState] = useState({
+    artistQuery: "",
+    artist: null,
+    tracks: [],
+  });
 
-  updateArtistQuery = event => {
-    this.setState({ artistQuery: event.target.value});
-  }
+  const updateArtistQuery = (event) => {
+    setState({ artistQuery: event.target.value });
+  };
 
-  handleKeyPress = event => {
-    if (event.key === 'Enter') {
-      this.searchArtist();
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      searchArtist();
     }
-  }
+  };
 
-  searchArtist = () => {
-    fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
-      .then(response => response.json())
-      .then(json => {
-        if (json.artists.total > 0)
+  const searchArtist = () => {
+    fetch(`${API_ADDRESS}/artist/${state.artistQuery}`)
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.artists.total > 0) {
           const artist = json.artists.items[0];
 
-          console.log('artist', artist);
-          this.setState({ artist });
+          console.log("artist", artist);
+          setState({ artist });
 
           fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
-            .then(response => response.json())
-            .then(json => this.setState({ tracks: json.tracks }))
-            .catch(error => alert(error.message));
+            .then((response) => response.json())
+            .then((json) => setState({ tracks: json.tracks }))
+            .catch((error) => alert(error.message));
         }
       })
-      .catch(error => alert(error.message));
-  }
+      .catch((error) => alert(error.message));
+  };
 
-  render() {
-    console.log('this state', this.state);
-
-    return (
-      <div>
+  return (
+    <div>
       <h2>Music Master</h2>
-      <input 
-      onChange={this.updateArtistQuery}
-      onKeyPress={this.handleKeyPress}
-      placeholder='Search for an Artist' 
+      <input
+        onChange={updateArtistQuery}
+        onKeyPress={handleKeyPress}
+        placeholder="Search for an Artist"
       />
-      <button onClick={this.searchArtist}>Search</button>
-      </div>
-    );
-  }
+      <button onClick={searchArtist}>Search</button>
+    </div>
+  );
 }
 
 export default App;
